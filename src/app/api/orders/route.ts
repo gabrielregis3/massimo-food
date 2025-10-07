000d7cc2fdfd360ff6ec4b30b3acc26ae49592ce
gabrielregis3 <txiloo3@gmail.com>
2025-10-07 16:52:08 -0300

    payment process using stripe

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -30,3 +30,30 @@ export const GET = async (req: NextRequest) => {
             { status: 401 });
     }
 }
+
+export const POST = async (req: NextRequest) => {
+
+    const session = await getAuthSession();
+
+    if (session) {
+        try {
+            const body = await req.json();
+
+            if (session.user) {
+                const order = await prisma.order.create({
+                    data: body
+                })
+
+                return new NextResponse(JSON.stringify(order), { status: 201 });
+            }
+        } catch (error) {
+            return new NextResponse(
+                JSON.stringify({ message: "Something went wrong!" }),
+                { status: 500 });
+        }
+    } else {
+        return new NextResponse(
+            JSON.stringify({ message: "Not authenticated!" }),
+            { status: 401 });
+    }
+}
